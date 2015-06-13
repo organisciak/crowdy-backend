81560baa76c21519f717cc0018d84acc6cf4c8ab
Peter Organisciak <organisciak@gmail.com>
2015-06-12 22:18:12 -0600

    Improved taskset model

--- models/taskset.js
+++ models/taskset.js
@@ -1,21 +1,29 @@
 var mongoose = require('mongoose');
-//mongoose.connect('mongodb://localhost/test');
 
 var taskSetSchema = mongoose.Schema({
-    turk_id: String,
-    user: String,
+    _id: String, // The HITId
+    user: String, //Should be a hashed version of WorkerId
     time:{
-        start: String,
-        end: String,
-        completion: String
+        start: String, //AcceptTime
+        submit: String, //SubmitTime
+        workTime: Number //WorkTimeInSeconds
     },
     feedback:{ 
         form: String,
         satisfaction: String
     },
-    tasks:[
-
-    ]
+    tasks:[{
+        //'type' is a reserved word, so when I want 'type'
+        // to actually be in my schema, it needs to be defined
+        // with { type: String }
+        type: {type: String},
+        item:{
+            type: {type: String},
+            id:Number
+        },
+        timeSpent: Number,
+        contribution:mongoose.Schema.Types.Mixed
+    }]
 });
 
 /* EXAMPLE
