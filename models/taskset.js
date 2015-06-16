33315b8702ea159475bad5fb65c2e5518e8c397e
Peter Organisciak <organisciak@gmail.com>
2015-06-15 23:08:47 -0600

    Add class methods

--- models/taskset.js
+++ models/taskset.js
@@ -1,9 +1,10 @@
 var mongoose = require('mongoose');
 
+// Define Model Schema
 var taskSetSchema = mongoose.Schema({
     _id: String, // The HITId
     user: String, //Should be a hashed version of WorkerId
-    hit_id: String, //My unique key for the HIT (Mongo HIT _id, not hitTypeID)
+    hit_id:String, //My unique key for the HIT (Mongo HIT _id, not hitTypeID)
     time:{
         start: String, //AcceptTime
         submit: String, //SubmitTime
@@ -19,7 +20,7 @@ var taskSetSchema = mongoose.Schema({
         // with { type: String }
         type: {type: String},
         item:{
-            type: {type: String},
+            type: { type: String },
             id:Number
         },
         timeSpent: Number,
@@ -27,6 +28,29 @@ var taskSetSchema = mongoose.Schema({
     }]
 });
 
-var TaskSet = mongoose.model('TaskSet', taskSetSchema);
+// Define Class Methods
+taskSetSchema.statics.countUserAll = function(user, callback) {
+    return this.count({user:user}).exec(callback);
+};
+
+taskSetSchema.statics.countUserHIT = function(user, hit_id, callback) {
+    return this.count({user:user, hit_id:hit_id}).exec(callback);
+};
+
+// List of all items a user can completed
+taskSetSchema.statics.userItemList = function(user, callback){
+    return this.aggregate([
+            {$match: {user:user}},
+            {$project:{"tasks.item.id":1}},
+            {$unwind : "$tasks"},
+            {$group: {_id:"$tasks.item.id"}}
+    ]).exec(callback);
+};
+
+// Define Instance Methods
+
+// Create Model and Export
+
+var TaskSet = mongoose.model('tasksets', taskSetSchema);
 
 module.exports = TaskSet; 
