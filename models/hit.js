295bbe5132a6b30354b4895879634ad58b7feade
Peter Organisciak <organisciak@gmail.com>
2015-07-13 21:07:07 -0600

    Comments on conditions

--- models/hit.js
+++ models/hit.js
@@ -3,7 +3,13 @@ var mongoose = require('mongoose');
 var hitSchema = mongoose.Schema({
     name: String,
     condition: { 
-        firstTask: String, // i.e. basic, training, fast
+        // i.e. basic, training, fast
+        firstTask: String,
+        // Note that 'feedback' is extra decoration on top of 
+        // a task, so specifying this condition will pair it based on
+        // the firstTask. If firstTask is 'fast', the feedback will be
+        // paired with the fast design; otherwise, it is paired with
+        // the basic design 
         laterTasks:String  // i.e. basic, feedback, training, fast
     },
     type: String, // e.g. Image tagging
@@ -13,6 +19,11 @@ var hitSchema = mongoose.Schema({
     itemModel: String,
     setsPerItem: Number,
     maxSetSize: Number,
+    // Used for estimating how long the task will take
+    // Not used for the 'fast' condition, because the timer
+    // time is the best estimate
+    itemTimeEstimate: Number,
+    // Specifies the task-completion timer in 'fast' condition
     timer: { type: Number, default: null },
 });
 
