2722ebf333c6655e7ab6adc18b4dc4aad7565d40
Peter Organisciak <organisciak@gmail.com>
2015-07-15 23:12:17 -0600

    Add payment info to db representation of HIT

--- models/hit.js
+++ models/hit.js
@@ -12,6 +12,15 @@ var hitSchema = mongoose.Schema({
         // the basic design 
         laterTasks:String  // i.e. basic, feedback, training, fast
     },
+    payment: Number, // In dollars
+    // Bonus, in dollars
+    bonus: {
+        task:Number,
+        // Nth array value corresponds to Nth task item. Last value will
+        // repeat if there are more items (so a constant bonus would be
+        // an array of length 1
+        item:[Number]
+    },
     type: String, // e.g. Image tagging
     items: [Number],
     // The name of the model for items, e.g. 'pin'.
@@ -24,7 +33,7 @@ var hitSchema = mongoose.Schema({
     // time is the best estimate
     itemTimeEstimate: Number,
     // Specifies the task-completion timer in 'fast' condition
-    timer: { type: Number, default: null },
+    timer: { type: Number, default: null }
 });
 
 var Hit = mongoose.connection.model('Hit', hitSchema);
